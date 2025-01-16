b90b2856f7593c8012adb174bcb96ba5ceccd605
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2025-01-17 01:40:31 +0800

    replace webp with mp4

--- src/components/Layouts/PortfolioSection.tsx
+++ src/components/Layouts/PortfolioSection.tsx
@@ -6,22 +6,21 @@ import Image from "next/image";
 
 const PortfolioSection = () => {
 	const [currentMovie, setCurrentMovie] = useState(0);
-
-	useEffect(() => {
-		console.log(movies[currentMovie].background);
-	}, [currentMovie]);
+	const [videoLoaded, setVideoLoaded] = useState(false);
 
 	const movies: {
 		title: string;
 		year: number;
-		background: string;
+		videoSrc: string;
+		thumbnail: string;
 		description: string;
 		alt?: string;
 	}[] = [
 		{
 			title: "Her Shadow’s Hold",
 			year: 2024,
-			background: "/assets/works/portfolio1.webp",
+			videoSrc: "/assets/works/portfolio1.mp4",
+			thumbnail: "/assets/works/portfolio1.png",
 			description:
 				"When a struggling artist is abducted by a mysterious and unhinged woman, he finds himself trapped in a nightmare where reality and obsession blur. As he uncovers her twisted motives, survival becomes a game of wits, and escaping her clutches could mean facing an even darker truth.",
 			alt: "Image of Her Shadow’s Hold",
@@ -29,7 +28,8 @@ const PortfolioSection = () => {
 		{
 			title: "Black Echo",
 			year: 2024,
-			background: "/assets/works/portfolio2.webp",
+			videoSrc: "/assets/works/portfolio2.mp4",
+			thumbnail: "/assets/works/portfolio2.png",
 			description:
 				"A special ops SWAT team is sent deep into enemy territory to extract a key informant holding secrets that could prevent global catastrophe. As alliances fracture and loyalties are tested, the team must confront not only their foes but also their own moral dilemmas in the fog of war.",
 			alt: "Image of Black Echo",
@@ -37,7 +37,8 @@ const PortfolioSection = () => {
 		{
 			title: "Echoes in Silence",
 			year: 2023,
-			background: "/assets/works/portfolio3.webp",
+			videoSrc: "/assets/works/portfolio3.mp4",
+			thumbnail: "/assets/works/portfolio3.png",
 			description:
 				"This gripping documentary unravels the chilling story of a serial killer who terrorized a small town. Through interviews with reporters, detectives, and the grieving families, Echoes in Silence pieces together the devastating impact of the crimes and the relentless pursuit of justice.",
 			alt: "Image of Echoes in Silence",
@@ -45,7 +46,8 @@ const PortfolioSection = () => {
 		{
 			title: "Heavy Lies the Crown",
 			year: 2023,
-			background: "/assets/works/portfolio4.webp",
+			videoSrc: "/assets/works/portfolio4.mp4",
+			thumbnail: "/assets/works/portfolio4.png",
 			description:
 				"From the gritty streets to the bright lights of the championship ring, Heavy Lies the Crown follows a determined young boxer who fights to overcome poverty, personal loss, and self-doubt. With grit and resilience, he rises against all odds to become a symbol of hope and perseverance.",
 			alt: "Image of Heavy Lies the Crown",
@@ -53,7 +55,8 @@ const PortfolioSection = () => {
 		{
 			title: "Neon Fragments",
 			year: 2022,
-			background: "/assets/works/portfolio5.webp",
+			videoSrc: "/assets/works/portfolio5.mp4",
+			thumbnail: "/assets/works/portfolio5.png",
 			description:
 				"Under the neon haze of a bustling Asian metropolis, lives intertwine in a story of longing, heartbreak, and fleeting connections. With its lush green hues and poetic atmosphere, Neon Fragments captures the beauty of solitude and the bittersweet moments that define human connection.",
 			alt: "Image of Neon Fragments",
@@ -61,8 +64,27 @@ const PortfolioSection = () => {
 	];
 
 	return (
-		<section id="works" className="w-full h-screen">
-			<Image src={`${movies[currentMovie].background}`} fill alt="" placeholder="empty" style={{ objectFit: "cover", zIndex: "-10" }} unoptimized />
+		<section id="works" className="w-full h-screen relative">
+			<Image
+				src={`${movies[currentMovie].thumbnail}`}
+				fill
+				alt={`${movies[currentMovie].alt}`}
+				className={cn(`w-full h-full object-cover z-[-7] transition-opacity duration-300 ease-in-out`, videoLoaded ? "opacity-0" : "opacity-100")}
+			/>
+			<video
+				autoPlay
+				loop
+				muted
+				playsInline
+				className={cn(`w-full h-full object-cover z-[-10] absolute `)}
+				src={`${movies[currentMovie].videoSrc}`}
+				onLoadStart={() => {
+					setVideoLoaded(false);
+				}}
+				onCanPlayThrough={() => {
+					setVideoLoaded(true);
+				}}
+			></video>
 			<div className="absolute inset-0 bg-gradient-to-t from-black to-transparent z-[-5]"></div>
 			<div className="w-full h-full flex flex-row sm:flex-col lg:flex-row items-end sm:items-start lg:items-end justify-between sm:justify-end px-6 sm:px-12 py-16 pt-32 gap-4">
 				<div id="movie-picker" className="flex flex-col w-fit">
